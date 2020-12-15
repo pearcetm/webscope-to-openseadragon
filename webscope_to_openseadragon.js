8989a92bae9f24a9eb8530d8c421d859cac2f6b9
pearcetm <tmpearce@gmail.com>
2020-12-15 16:15:11 -0500

    Update webscope_to_openseadragon.js

--- webscope_to_openseadragon.js
+++ webscope_to_openseadragon.js
@@ -1,6 +1,6 @@
 /*
 To apply this script to a page running Aperio WebViewer, paste the line below into the browser's debugger console
-var s = document.createElement('script');s.src="https://pearcetm.github.io/webscope_to_openseadragon/webscope_to_openseadragon.js";document.head.append(s);
+var s = document.createElement('script');s.src="https://pearcetm.github.io/webscope-to-openseadragon/webscope_to_openseadragon.js";document.head.append(s);
 
 */
 var openseadragonImagePath = 'https://pearcetm.github.io/webscope-to-openseadragon/images/';
